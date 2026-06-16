b5ea5b2f00378c065ee1bbd29f2016fdac5c1331
OloaneShark <oloanedennis0@gmail.com>
2026-06-16 15:11:14 -0400

    Set up CloudGuard project structure

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 
-##Test
+import boto3
+
+def test_boto3():
+    print('boto3 successfully imported!')
+    
+    try:
+        session = boto3.Session()
+        region = session.region_name
+        print(f"AWS configuration found. Default region {region}")
+    
+    except Exception as e:
+        print("boto3 is imported, but no default AWS region was detected")
+        
+if __name__ == "__main__":
+    test_boto3()
