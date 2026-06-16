b189f6490c91f79135e5aaa197a9a0e321c7496a
OloaneShark <oloanedennis0@gmail.com>
2026-06-16 15:14:53 -0400

    Add initial scanner module

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
 
 import boto3
+from scanner import run_scan
 
-def test_boto3():
-    print('boto3 successfully imported!')
-    
-    try:
-        session = boto3.Session()
-        region = session.region_name
-        print(f"AWS configuration found. Default region {region}")
-    
-    except Exception as e:
-        print("boto3 is imported, but no default AWS region was detected")
-        
-if __name__ == "__main__":
-    test_boto3()
+print("Cloudguard Security Scan")
+print()
+
+run_scan()
