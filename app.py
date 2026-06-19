4a102450ed3536f243bf8907e221b9ec7c75e79d
OloaneShark <oloanedennis0@gmail.com>
2026-06-19 12:46:05 -0400

    Add Flask dashboard for scan reports

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 """AWS Bucket implemented on June 17, 2026 at 9:47 am est"""
 
-
-import boto3
-from scanner import list_s3_buckets
-
-
-print("CloudGuard Security Scan")
-print("Checking S3 buckets...")
-print()
+from flask import Flask, render_template
+import json
+import os
+import glob
 
 
-list_s3_buckets()
+app = Flask(__name__)
+
+def get_latest_report():
+    report_files = glob.glob("reports/*.json")
+    
+    if not report_files:
+        return[]
+    
+    latest_report = max(report_files, key=os.path.getctime)
+    
+    with open(latest_report, "r") as file:
+        return json.load(file)
+    
+    
+@app.route("/")
+def dashboard():
+    report_data = get_latest_report()
+    return render_template("dashboard.html", report_data=report_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
